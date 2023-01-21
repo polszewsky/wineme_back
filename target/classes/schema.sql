4668b1740610c2d88a5381df708af992080aa5b5
polszewsky <32812297+polszewsky@users.noreply.github.com>
2023-01-21 12:19:33 +0100

    clean up

--- target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
--- no schema here
-
-ALTER TABLE public.adm_privilege OWNER TO postgres;
-ALTER TABLE public.adm_role OWNER TO postgres;
-ALTER TABLE public.roles_privileges OWNER TO postgres;
-ALTER TABLE public.adm_user OWNER TO postgres;
